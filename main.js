43cf07d6f1ab9fe570069e2def937026f04ec32a
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2021-03-09 08:50:14 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,77 @@
+var RangeDownloader = (function() {
+    var RangeDownloader = function (param) {
+        this.url = param.url || null;
+        if(!this.url){throw Error("Invaild url.");}
+        this.onerror = param.onerror || null;
+        this.onload = param.onload || null;
+        this.onprogress = param.onprogress || null;
+    };
+    RangeDownloader.prototype.start = function () {
+        if(this.loaded){return;}
+        var _self = this;
+        this.abortController = new AbortController();
+        fetch(this.url,{
+            headers: {
+                "Range": "bytes="+this.downloadedSize.toString() + "-"
+            },
+            signal: this.abortController.signal,
+        }).then(function (resp){
+
+            if(resp.status == 206){
+                _self.supportPartial = true;
+            }
+            _self.running = true;
+            return resp.body.getReader();
+        }).then(async function (reader){
+            var ok = false;
+            while(!ok){
+                var con = await reader.read();
+                ok = con.done;
+                if(typeof con.value == "undefined"){break;}
+                _self.chucks.push(con.value);
+                _self.downloadedSize += con.value.byteLength;
+                typeof _self.onprogress == "function" ? _self.onprogress(_self) : 0;
+            }
+            if(ok){
+                typeof _self.onload == "function" ? _self.onload(_self) : 0;
+            }
+            _self.running = false;
+            _self.loaded = true;
+        }).catch(function (err){
+            if(_self.running == true) {
+                throw Error(err);
+                typeof _self.onerror == "function" ? _self.onerror(_self,err) : 0;
+            }
+            _self.running = false;
+        });
+    }
+    RangeDownloader.prototype.pause = function () {
+        this.running = false;
+        this.abortController != null ? this.abortController.abort() : 0;
+        this.chucks = this.supportPartial ? this.chucks : [];
+        this.downloadedSize = this.supportPartial ? this.downloadedSize : 0;
+    }
+    RangeDownloader.prototype.cancel = function () {
+        this.running = false;
+        this.abortController != null ? this.abortController.abort() : 0;
+        this.chucks = [];
+        this.downloadedSize = 0;
+        this.totalSize = 0;
+        this.supportPartial = false;
+    }
+    RangeDownloader.prototype.getResultAsBlob = function (release) {
+        return new Blob(this.chucks);
+    }
+    RangeDownloader.prototype.running = false;
+    RangeDownloader.prototype.url = "";
+    RangeDownloader.prototype.onerror = null;
+    RangeDownloader.prototype.onprogress = null;
+    RangeDownloader.prototype.onload = null;
+    RangeDownloader.prototype.supportPartial = false;
+    RangeDownloader.prototype.chucks = [];
+    RangeDownloader.prototype.totalSize = 0;
+    RangeDownloader.prototype.downloadedSize = 0;
+    RangeDownloader.prototype.abortController = null;
+    RangeDownloader.prototype.loaded = false;
+    return RangeDownloader;
+})();
